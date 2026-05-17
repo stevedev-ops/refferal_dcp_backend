ff7a221d148c674b124d2306b35a1d043a82db07
stevedev-ops <smutwiri9250@gmail.com>
2026-05-17 10:15:24 +0300

    Run migrations programmatically in WSGI to ensure production DB is migrated on startup

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+django.setup()
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Automatic migration failed: {e}")
 
 application = get_wsgi_application()
